75af5d9214af0d65ff7b7692141549d8397a2942
Gino Ureta <ing.gino@gmail.com>
2021-08-26 23:29:00 -0400

    Classes

---
+++ GroovyBasics/src/G03_Classes.groovy
@@ -0,0 +1,12 @@
+//Unlike java, files and the class itself can have different names.
+//e.g. this file is called: "G03_Classes" but the containing class is "AngryBirds"
+//But is recommended to follow same name for convention.
+
+class AngryBirds {
+
+}
+
+//Also there might be more than one class declared
+class Bird{}
+class Pig{}
+// A file with no classes is basically a script.
--- GroovyBasics/src/training/HelloWorld.groovy
+++ GroovyBasics/src/training/HelloWorld.groovy
@@ -2,3 +2,4 @@ package training
 
 println "Hello World!"
 
+// When creating a script, Groovy internally creates a java class and run from it.
