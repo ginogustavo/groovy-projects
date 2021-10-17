b0486111747c0400c42c9efca6560cb5eaed0afc
Gino Ureta <ing.gino@gmail.com>
2021-10-17 07:58:41 -0400

    regex reference

--- 02_DataTypes/G07_Regex.groovy
+++ 02_DataTypes/G07_Regex.groovy
@@ -12,34 +12,34 @@ String url= $/http://groovy.com/blog/$
 
 println slashy.class  //java.lang.String
 
-def pattern = ~/a\b/  //the ~ replaces the Pattern.compile
-println pattern.class // java.util.regex.Pattern
+def pattern2 = ~/a\b/  //the ~ replaces the Pattern.compile
+println pattern2.class // java.util.regex.Pattern
 
 // Find | Match
 
 def text = "Being a Bears sports fan is no way to go through life" // true
-def pattern = ~/Bears sports fan/
-def finder = text =~ pattern    //It reads: in that text can we find this pattern : false
-def matcher = text ==~ pattern
+def pattern3 = ~/Bears sports fan/
+def finder = text =~ pattern3    //It reads: in that text can we find this pattern : false
+def matcher = text ==~ pattern3
 
 println finder// it will returns instance of java.util.regex.Matcher
 println finder.size()
 
 
 
-def text = "Bears sports fan"
-def pattern = ~/Bears sports fan/
-def matcher = text ==~ pattern
+def text4 = "Bears sports fan"
+def pattern4 = ~/Bears sports fan/
+def matcher4 = text4 ==~ pattern4
 
-println matcher // true
+println matcher4 // true
 
 if (matcher){} // do something
 
 
-def text = "Being a Bears sports fan is no way to go through life"
-def pattern = ~/Bears/
+def text5 = "Being a Bears sports fan is no way to go through life"
+def pattern5 = ~/Bears/
 
-text = text.replaceFirst(pattern, "Buffalo")
-println text
+text5 = text5.replaceFirst(pattern5, "Buffalo")
+println text5
 
 //https://www.regular-expressions.info/refquick.html
