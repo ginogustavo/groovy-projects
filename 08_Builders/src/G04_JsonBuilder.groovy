ca7779549938bad5af7e1bd76f20eb5ba6319e26
Gino Ureta <ing.gino@gmail.com>
2021-11-15 04:24:41 -0500

    JsonBulder

---
+++ 08_Builders/src/G04_JsonBuilder.groovy
@@ -0,0 +1,23 @@
+import groovy.json.JsonBuilder
+
+JsonBuilder builder = new JsonBuilder()
+builder.books{
+
+    currentBook{
+        title 'The 4 hour work week'
+        isbn '978-0-307-46535-1'
+        author (first:'Timothy', last: 'Ferris',twitter:"@tferris" )
+        related 'the 4 hour body', 'the 4 hour chef'
+    }
+
+    nextBook{
+        title '#AskGaryLee'
+        isbn '978-0-345-741569-1'
+        author (first:'Gary', last: 'Vay',twitter:"@gary" )
+        related 'Jab jab jab Right hook', 'crush it'
+    }
+
+}
+println builder.toString()
+println builder.toPrettyString()
+new File('json/books.json').write(builder.toPrettyString())
